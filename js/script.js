2a6e22353ecc4a47410aace0c6834b5337c68015
antonius1980 <antonius1980@gmail.com>
2025-09-25 14:51:35 +0300

    Updating base files.Started the first task.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,38 @@
 'use strict';
-const test = navigator.login;
-//console.log(test);
 
 const test2 = document.body;
-const test3 = test2.classList.value;
 
-console.log(test3);
+const playList = [
+{
+ author: "LED ZEPPELIN",
+ song:"STAIRWAY TO HEAVEN"
+},
+{
+ author: "QUEEN",
+ song:"BOHEMIAN RHAPSODY"
+},
+{
+ author: "LYNYRD SKYNYRD",
+ song:"FREE BIRD"
+},
+{
+ author: "DEEP PURPLE",
+ song:"SMOKE ON THE WATER"
+},
+{
+ author: "JIMI HENDRIX",
+ song:"ALL ALONG THE WATCHTOWER"
+},
+{
+ author: "AC/DC",
+ song:"BACK IN BLACK"
+},
+{
+ author: "QUEEN",
+ song:"WE WILL ROCK YOU"
+},
+{
+ author: "METALLICA",
+ song:"ENTER SANDMAN"
+}
+];
